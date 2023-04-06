9b8c412a2a8d118dc1efec121d0ace54c6023190
Rohil <125549822+Rohil-Solutelabs@users.noreply.github.com>
2023-04-06 16:51:19 +0530

    Update 1.js

--- 1.js
+++ 1.js
@@ -4,18 +4,20 @@
 // Note that the target sum has to be obtained by summing two different integers in the array; you can’t add a single integer to itself to obtain the target sum.
 // You can assume that there will be at most one pair of numbers summing up to the target sum.
 
-function findTwoSumnew(arr, target) {
+function findTwoSum(arr, target) {
   const result = [];
 
   for (let i = 0; i < arr.length; i++) {
     for (let j = i + 1; j < arr.length; j++) {
       if (arr[i] + arr[j] == target) {
         result.push(arr[i], arr[j]);
+        break; 
       }
     }
+    if (result.length > 0) break; 
   }
   return result;
 }
 
-const arr = [3, 5, -4, 8, 24, 11, 1, -1, 6, -5, 10, 12, -34, 15];
-console.log(findTwoSumnew(arr, -23));
+const arr = [3, 5, -4, 8, 24, 11, 1, 4, 2, 2, -1, 6, -5, 10, 12, -34, 15];
+console.log(findTwoSum(arr, 16));
